08d25021494f902d14b164d444e9fb6c55655ff4
hannahwilbert <hannahclaire230@yahoo.com>
2019-03-31 22:47:03 -0700

    starting

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => (
+
+<footer className="footer bg-info">
+            Clicky Game!! by Eric Jia
+</footer>
+
+)
+export default Footer;
